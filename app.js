d61d73e4cd43e10317751101645924b968190dce
Stas Tolmachev <stusik@yandex.ru>
2023-06-12 15:13:36 +0300

    0.15

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const router = require("./routes");
-const path = require('path');
+const path = require("path");
 
 const { PORT = 3000 } = process.env;
 mongoose
@@ -15,17 +15,23 @@ const app = express();
 
 app.use((req, res, next) => {
   req.user = {
-    _id: '648582273b35f8daa0eca946' // вставьте сюда _id созданного в предыдущем пункте пользователя
+    _id: "648582273b35f8daa0eca946", // вставьте сюда _id созданного в предыдущем пункте пользователя
   };
 
   next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.json());
 
 app.use(router);
+
+app.use((err, req, res) => {
+  if (err.status === 404) {
+    res.status(404).send({ message: "Страница не найдена" });
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Server started on port 3000");
