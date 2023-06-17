816784fac25e14d247299374799d830f98854d6d
Stas Tolmachev <stusik@yandex.ru>
2023-06-17 08:04:16 +0300

    0.23

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes');
 
+const app = express();
+const NOT_FOUND = 404;
+
 const { PORT = 3000 } = process.env;
+
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestodb', {
     useNewUrlParser: true,
   })
   .then(() => console.log('MongoDB connected'));
-
-const app = express();
 
 app.use((req, res, next) => {
   req.user = {
@@ -24,7 +26,7 @@ app.use(express.json());
 app.use(router);
 
 app.patch('*', (req, res) => {
-  res.status(404).send({ message: 'Page not found' });
+  res.status(NOT_FOUND).send({ message: 'Page not found' });
 });
 
 app.listen(PORT, () => {
