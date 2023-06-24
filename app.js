06f756fb42f82f421ca59c3977078a00db6ade36
Stas Tolmachev <stusik@yandex.ru>
2023-06-24 14:34:18 +0300

    0.25

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 const auth = require('./middlewares/auth');
 const router = require('./routes');
 const { login, createUser } = require('./controllers/users');
@@ -20,8 +21,32 @@ app.use(express.json());
 
 app.use(router);
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  login,
+);
+
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().pattern(
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, // eslint-disable-line
+      ),
+      email: Joi.string().required(),
+      password: Joi.string().required().min(8),
+    }),
+  }),
+  createUser,
+);
 
 app.patch('*', (req, res) => {
   res.status(NOT_FOUND).send({ message: 'Page not found' });
